888818b5bdfc2fdc2ae9e0071a9cd2195166e00a
Fernando Vega <58368168+normacocsub@users.noreply.github.com>
2023-12-25 23:34:15 -0500

    Actualizar index.tsx

--- pages/registro/index.tsx
+++ pages/registro/index.tsx
@@ -2,6 +2,9 @@ import Layout from "../../components/layout"
 import UserRegister from "../../components/user_register"
 
 const Registro = () => {
+    const Test = () => { 
+        console.log("test")
+    } 
     return <Layout>
         <UserRegister title="Registro Usuario" />
     </Layout>
