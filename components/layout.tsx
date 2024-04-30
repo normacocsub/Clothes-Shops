7dc6545310642bb699eacb6689292eaab4c8ffc6
Fernando Vega <58368168+normacocsub@users.noreply.github.com>
2024-04-29 22:45:12 -0500

    Update

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,16 +2,12 @@ import { FunctionComponent } from 'react';
 import styles from '../styles/layout.module.scss';
 import Header from './header';
 import { Analytics } from '@vercel/analytics/react';
-import Head from 'next/head';
 
 type LayoutProps = {
     children: any
   }
 const Layout: FunctionComponent<LayoutProps> = ({children}) => {
     return <div className={styles.contenedorMain}>
-        <Head>
-            <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-        </Head>
         <div className={styles.header}>
             <Header />
         </div>
